9de402601d5b48319c09f2289d267565d2b22994
Jeff Sember <jpsember@gmail.com>
2015-03-06 10:24:22 -0800

    Removed unnecessary element from test manifest file

--- ASkeletonTest/src/com/js/askeleton/test/ToolsTest.java
+++ ASkeletonTest/src/com/js/askeleton/test/ToolsTest.java
@@ -1,14 +1,12 @@
 package com.js.askeleton.test;
 
-import com.js.basic.Tools;
 import com.js.testUtils.*;
 import static com.js.basic.Tools.*;
 
 public class ToolsTest extends MyTestCase {
 
 	public void testSnapshot() {
-		Tools.ASSERT(true);
-		IOSnapshot.open(false);
+		IOSnapshot.open();
 		pr("Snapshot test");
 		IOSnapshot.close();
 	}

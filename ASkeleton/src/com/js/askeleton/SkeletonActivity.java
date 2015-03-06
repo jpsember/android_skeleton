4185d3dd8bf497e3e357d844e8aaa69ff233b0f9
Jeff Sember <jpsember@gmail.com>
2015-03-06 10:54:32 -0800

    Got rid of UserEvent and related classes

--- ASkeleton/src/com/js/askeleton/SkeletonActivity.java
+++ ASkeleton/src/com/js/askeleton/SkeletonActivity.java
@@ -1,9 +1,6 @@
 package com.js.askeleton;
 
 import com.js.android.MyActivity;
-import com.js.android.TouchEventGenerator;
-import com.js.basic.Point;
-import com.js.basic.UserEventSource;
 
 import android.graphics.Color;
 import android.os.Bundle;
@@ -19,19 +16,8 @@ public class SkeletonActivity extends MyActivity {
 	}
 
 	private View prepareView() {
-
 		View view = new View(this);
 		view.setBackgroundColor(Color.GREEN);
-
-		mTouchEventGenerator = new TouchEventGenerator();
-		mTouchEventGenerator.setView(new UserEventSource() {
-			@Override
-			public Point viewToWorld(Point viewPt) {
-				return new Point(viewPt);
-			}
-		}, view);
 		return view;
 	}
-
-	private TouchEventGenerator mTouchEventGenerator;
 }

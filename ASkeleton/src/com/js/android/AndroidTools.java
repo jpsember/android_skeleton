9d983e813181f23e430dc81ad14622258d625de9
Jeff Sember <jpsember@gmail.com>
2015-03-30 13:23:31 -0700

    Incorporated changes from gest, geometry projects

---
+++ ASkeleton/src/com/js/android/AndroidTools.java
@@ -0,0 +1,120 @@
+package com.js.android;
+
+import java.io.InputStream;
+import java.io.PrintStream;
+
+import com.js.basic.Files;
+
+import android.content.Context;
+import android.widget.Toast;
+import static com.js.basic.Tools.*;
+
+public final class AndroidTools {
+
+  /**
+   * A do-nothing method that can be called to avoid 'unused import' warnings
+   * related to this class
+   */
+  public static final void doNothingAndroid() {
+  }
+
+  /**
+   * Display a toast message
+   */
+  public static void toast(Context context, String message, int duration) {
+    Toast toast = Toast.makeText(context, message, duration);
+    toast.show();
+  }
+
+  /**
+   * Display a toast message of short duration
+   */
+  public static void toast(Context context, String message) {
+    toast(context, message, Toast.LENGTH_SHORT);
+  }
+
+  /**
+   * Display toast message describing an exception
+   * 
+   * @param context
+   * @param throwable
+   *          exception received
+   * @param optional
+   *          message message to display within toast (may be shown with
+   *          exception message as well)
+   */
+  public static void showException(Context context, Throwable exception,
+      String message) {
+    warning("caught: " + exception);
+    if (message == null)
+      message = "Caught";
+    toast(context, message + ": " + exception, Toast.LENGTH_LONG);
+  }
+
+  public static String readTextFileResource(Context context, int resourceId) {
+    String str = null;
+    try {
+      InputStream stream = context.getResources().openRawResource(resourceId);
+      str = Files.readString(stream);
+    } catch (Throwable e) {
+      die("problem reading resource #" + resourceId, e);
+    }
+    return str;
+  }
+
+  public static String getStringResource(Context context, String stringName) {
+    String packageName = context.getPackageName();
+    int resId = context.getResources().getIdentifier(stringName, "string",
+        packageName);
+    String str = null;
+    if (resId != 0)
+      str = context.getString(resId);
+    if (str == null)
+      throw new IllegalArgumentException("string name " + stringName
+          + "  has resource id " + resId + ", no string found");
+    return str;
+  }
+
+  public static String applyStringSubstitution(Context context, String s) {
+    if (s.startsWith("@")) {
+      s = getStringResource(context, s.substring(1));
+    }
+    return s;
+  }
+
+  /**
+   * Install a filter on System.out so multiple linefeeds are NOT filtered by
+   * the Android logger.
+   */
+  static class AndroidSystemOutFilter {
+
+    private static boolean alreadyInstalled;
+
+    public static void install() {
+      if (!alreadyInstalled) {
+        if (isAndroid())
+          System.setOut(new OurFilter());
+        alreadyInstalled = true;
+      }
+    }
+
+    private static class OurFilter extends PrintStream {
+      public OurFilter() {
+        super(System.out, true);
+      }
+
+      // This seems to be the only method I need to override...
+      @Override
+      public void write(byte[] buf, int off, int len) {
+        for (int i = 0; i < len; i++) {
+          byte k = buf[off + i];
+          if (k == '\n') {
+            super.write(' ');
+          }
+          super.write(k);
+        }
+      }
+    }
+  }
+
+}
--- ASkeleton/src/com/js/android/AppPreferences.java
+++ ASkeleton/src/com/js/android/AppPreferences.java
@@ -12,115 +12,113 @@ import android.content.SharedPreferences;
  */
 public class AppPreferences {
 
-	/**
-	 * No objects are to be constructed
-	 */
-	private AppPreferences() {
-	}
+  /**
+   * No objects are to be constructed
+   */
+  private AppPreferences() {
+  }
 
-	/**
-	 * This should be called once before any other methods
-	 * 
-	 * @param context
-	 *            application or activity context (treated differently in test
-	 *            mode)
-	 */
-	public synchronized static void prepare(Context context) {
-		if (preferences != null)
-			return;
+  /**
+   * This should be called once before any other methods
+   * 
+   * @param context
+   *          application or activity context (treated differently in test mode)
+   */
+  synchronized static void prepare(Context context) {
+    if (sPreferences != null)
+      return;
 
-		if (context instanceof Activity) {
-			preferences = ((Activity) context)
-					.getPreferences(Context.MODE_PRIVATE);
-		} else {
-			preferences = context.getSharedPreferences("__RBuddyApp_test_",
-					Context.MODE_PRIVATE);
-		}
-	}
+    if (context instanceof Activity) {
+      sPreferences = ((Activity) context).getPreferences(Context.MODE_PRIVATE);
+    } else {
+      sPreferences = context.getSharedPreferences("__test_preferences__",
+          Context.MODE_PRIVATE);
+    }
+  }
 
-	public static int getUniqueIdentifier(String key) {
-		synchronized (preferences) {
-			int value = preferences.getInt(key, 1000);
-			preferences.edit().putInt(key, 1 + value).commit();
-			return value;
-		}
-	}
+  public static int getUniqueIdentifier(String key) {
+    synchronized (sPreferences) {
+      int value = sPreferences.getInt(key, 1000);
+      sPreferences.edit().putInt(key, 1 + value).commit();
+      return value;
+    }
+  }
 
-	/**
-	 * Read string from app preferences
-	 * 
-	 * @param key
-	 * @param defaultValue
-	 * @return
-	 */
-	public static String getString(String key, String defaultValue) {
-		return preferences.getString(key, defaultValue);
-	}
+  /**
+   * Read string from app preferences
+   * 
+   * @param key
+   * @param defaultValue
+   * @return
+   */
+  public static String getString(String key, String defaultValue) {
+    return sPreferences.getString(key, defaultValue);
+  }
 
-	/**
-	 * Store string
-	 * 
-	 * @param key
-	 * @param value
-	 */
-	public static void putString(String key, String value) {
-		preferences.edit().putString(key, value).commit();
-	}
+  /**
+   * Store string
+   * 
+   * @param key
+   * @param value
+   */
+  public static void putString(String key, String value) {
+    sPreferences.edit().putString(key, value).commit();
+  }
 
-	public static void putInt(String key, int value) {
-		preferences.edit().putInt(key, value).commit();
-	}
+  public static void putInt(String key, int value) {
+    sPreferences.edit().putInt(key, value).commit();
+  }
 
-	public static int getInt(String key, int defaultValue) {
-		return preferences.getInt(key, defaultValue);
-	}
+  public static int getInt(String key, int defaultValue) {
+    return sPreferences.getInt(key, defaultValue);
+  }
 
-	/**
-	 * Read boolean from app preferences
-	 * 
-	 * @param key
-	 * @param defaultValue
-	 * @return
-	 */
-	public static boolean getBoolean(String key, boolean defaultValue) {
-		return preferences.getBoolean(key, defaultValue);
-	}
+  /**
+   * Read boolean from app preferences
+   * 
+   * @param key
+   * @param defaultValue
+   * @return
+   */
+  public static boolean getBoolean(String key, boolean defaultValue) {
+    return sPreferences.getBoolean(key, defaultValue);
+  }
 
-	/**
-	 * Store boolean
-	 * 
-	 * @param key
-	 * @param value
-	 */
-	public static void putBoolean(String key, boolean value) {
-		preferences.edit().putBoolean(key, value).commit();
-	}
+  /**
+   * Store boolean
+   * 
+   * @param key
+   * @param value
+   */
+  public static void putBoolean(String key, boolean value) {
+    sPreferences.edit().putBoolean(key, value).commit();
+  }
 
-	/**
-	 * Store a series of string values (an optimization to allow a single commit
-	 * at the end)
-	 * 
-	 * @param map
-	 *            map with string values
-	 */
-	public static void putStrings(Map<String, String> map) {
-		SharedPreferences.Editor editor = preferences.edit();
-		for (Map.Entry<String, String> entry : map.entrySet()) {
-			editor.putString(entry.getKey(), entry.getValue());
-		}
-		editor.commit();
-	}
+  /**
+   * Store a series of string values (an optimization to allow a single commit
+   * at the end)
+   * 
+   * @param map
+   *          map with string values
+   */
+  public static void putStrings(Map<String, String> map) {
+    SharedPreferences.Editor editor = sPreferences.edit();
+    for (Map.Entry<String, String> entry : map.entrySet()) {
+      editor.putString(entry.getKey(), entry.getValue());
+    }
+    editor.commit();
+  }
 
-	public static void removeKey(String preferencesKey) {
-		preferences.edit().remove(preferencesKey).commit();
-	}
+  public static void removeKey(String preferencesKey) {
+    sPreferences.edit().remove(preferencesKey).commit();
+  }
 
-	public static boolean toggle(String preferencesKey) {
-		boolean val = !getBoolean(preferencesKey, false);
-		putBoolean(preferencesKey, val);
-		return val;
-	}
+  public static boolean toggle(String preferencesKey) {
+    boolean val = !getBoolean(preferencesKey, false);
+    putBoolean(preferencesKey, val);
+    return val;
+  }
 
-	private static SharedPreferences preferences;
+  private static SharedPreferences sPreferences;
 
 }
